968a95b3d529b06003d34865ac98842aa3bbb513
rkx1209 <rkx1209dev@gmail.com>
2018-07-16 21:32:03 +0900

    Remove test directory

--- test/include/libtransistor/ipc/sm.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include<libtransistor/types.h>
-
-result_t sm_init();
-result_t sm_get_service(ipc_object_t *session, char *name);
-void sm_finalize();
--- test/include/libtransistor/tls.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void *get_tls();
